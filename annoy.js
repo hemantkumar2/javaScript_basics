e4b17d21bb8e3cd3bbceaa791f22be09d58b8b20
hemantkumar2 <50173564+hemantkumar2@users.noreply.github.com>
2019-06-17 01:48:14 +0530

    Add files via upload

---
+++ annoy.js
@@ -0,0 +1,5 @@
+var answer = prompt("Are we annoyed yet");
+while (answer !== "yes" && answer !== "YES" && answer !== "Yes" && answer !== "yeah" && answer !== "Yeah" && answer !== "YEAH" && answer !== "yep") {
+    var answer = prompt("Are we annoyed yet");
+}
+alert("YAY, WE MADE IT!!");
