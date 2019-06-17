94350c586af2e4739db67e66aeb555ee505b234a
hemantkumar2 <50173564+hemantkumar2@users.noreply.github.com>
2019-06-17 10:31:14 +0530

    Add files via upload

--- for_loop.js
+++ for_loop.js
@@ -9,25 +9,25 @@ for (var i = 0; i < str.length; i++) {
     console.log(str[i]);
 }
 console.log("Printing all the numbers from -10 to 19");
-for (var i = -10; i < 19; i++) {
+for (var i = -10; i <= 19; i++) {
     console.log(i);
 }
 console.log("Printing all the even numbers from 10 to 40");
-for (var i = 10; i < 40; i++) {
+for (var i = 10; i <= 40; i++) {
     if (i % 2 == 0) {
         console.log(i);
     }
 }
 
 console.log("Printing all the odd numbers from 300 to 333");
-for (var i = 300; i < 333; i++) {
+for (var i = 300; i <= 333; i++) {
     if (i % 2 == 1) {
         console.log(i)
     }
 }
 
 console.log("Printing all numbers which are divisible by 3 and 5, form 5 to 50 ");
-for (var i = 5; i < 50; i++) {
+for (var i = 5; i <= 50; i++) {
     if (i % 5 == 0 && i % 3 == 0) {
         console.log(i);
     }
