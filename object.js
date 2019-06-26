db4f54da1e0aabbcbe665ba8299c24c32886bf29
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-06-27 01:38:32 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,25 @@
+var person ={
+    name: "hemant",
+    age: 25,
+    city:"Bilaspur"
+};
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+
+
+
+// array and object 
+
+var posts = [
+{
+    title:"Cats are mediocre",
+    author:"Colt",
+    comments:["Awesome post","terrible post"]
+}
+{
+    title:"Cats are awesome",
+    author:"cat lover",
+    comment:["Awesome post","get lost I hate you"]
+}
+]
